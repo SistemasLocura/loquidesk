6287772f2b90652c9d9cc1932c4967b7c4481b44
juanchaco199622 <juan199622@hotmail.com>
2020-01-16 17:52:31 -0500

    update firebase integrando

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebaseConfig from './firebase.js'
+import{
+    FirebaseAppProvider
+} from 'reactfire';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <App />
+</FirebaseAppProvider>
+
+), document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
